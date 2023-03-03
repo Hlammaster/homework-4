7065365e1cd2874ad2685c187eed3fba0a01f16a
Johnny Firo <hlammaster@mail.ru>
2023-03-03 18:26:51 +0300

    added DragAndDropTest

---
+++ src/test/java/DragDropTest.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragDropTest {
+
+    @Test
+    void DragandDropTest(){
+      open("https://the-internet.herokuapp.com/drag_and_drop");
+      $("#column-a").dragAndDropTo("#column-b");
+      $("#column-a").shouldHave(Condition.text("B"));
+      $("#column-b").shouldHave(Condition.text("A"));
+
+
+
+
+    }
+}
